8ab9a2177dbf8d39bb0449297cdf26b5c453cb43
Yew12347 <98322702+Yew12347@users.noreply.github.com>
2024-07-28 18:01:53 +0700

    Update compile-c.sh

--- compile-c.sh
+++ compile-c.sh
@@ -1,9 +1,9 @@
 # compile-c-code.sh
 #!/bin/bash
 
-# Get WebKitGTK include and library paths
-WEBKIT_INCLUDE_PATH=$(flatpak info --show-location org.webkit.WebKitGtk)/include
-WEBKIT_LIB_PATH=$(flatpak info --show-location org.webkit.WebKitGtk)/lib
+# Set paths to WebKitGTK libraries and include files
+WEBKIT_INCLUDE_PATH=/path/to/WebKit/Source/WebKit
+WEBKIT_LIB_PATH=/path/to/WebKit/WebKitBuild/Release
 
 # Compile engine.c into a shared library
 gcc -shared -o libwebkit_browser.so -fPIC engine.c -I${WEBKIT_INCLUDE_PATH} -L${WEBKIT_LIB_PATH} -lwebkit2gtk-4.0 -lgtk-3
